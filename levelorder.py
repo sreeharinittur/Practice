38db3d4fd0a8e584302a6cba60d73d19d457a13e
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2024-12-24 22:15:57 +0530

    Add files via upload

---
+++ levelorder.py
@@ -0,0 +1,25 @@
+import collections
+class TreeNode:
+    def __init__(self,val,left,right):
+        self.val=val
+        self.left=left
+        self.right=right
+class Solution:
+    def levelorder(self,root:TreeNode):
+        res=[]
+        q=collections.deque()
+        q.append(root)
+        while q:
+            qlen=len(q)
+            level=[]
+            for i in range(qlen):
+                node=q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+                if level:
+                    res.append(level)
+        return res
+
+        
