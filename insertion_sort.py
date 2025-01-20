72879ebaf56f2db7a17522b9613ccf8302edb905
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-01-20 21:34:52 +0530

    Add files via upload

---
+++ insertion_sort.py
@@ -0,0 +1,12 @@
+a=[10,9,8,-50,50]
+def i_sort(a):
+    for i in range(1,len(a)):
+        
+        for j in range(i):
+            if a[j]>a[i]:
+                a[i],a[j]=a[j],a[i]
+
+
+    return a
+print(i_sort(a))
+    
