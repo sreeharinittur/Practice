69766dd0dc3734527c4551e3dc15a068cc5bcdf4
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-01-21 22:25:40 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class Array{
+    public static void main(String[] args) {
+        System.out.println("hii");
+        int[] n={1,2,3};
+        int a=n.length;
+        n[a]=4;
+        System.out.println(Arrays.toString(n));
+    }
+}
