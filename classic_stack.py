57797ffc54d259be4cff48686ced8312a2db25fb
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-01-22 21:41:10 +0530

    Add files via upload

---
+++ classic_stack.py
@@ -0,0 +1,20 @@
+class valid:
+    def is_valid(self,s):
+        stack=[]
+        for i in s:
+            if i=="(":
+                stack.append(i)
+            elif i==")":
+                if stack and stack[-1]=="(":
+                    stack.pop()
+                else:
+                    return False
+            else:
+                continue
+        if len(stack)==0:
+            return True
+        else:
+            return False
+v=valid()
+s="(PQR))"
+print(v.is_valid(s))
