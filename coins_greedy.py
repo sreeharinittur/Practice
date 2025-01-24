779f10a95a1193adba14ad20fb3aa2bb73de41a3
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-01-24 12:52:15 +0530

    Add files via upload

---
+++ coins_greedy.py
@@ -0,0 +1,13 @@
+def calc(coins,target):
+    coins2=sorted(coins)
+    res=0
+    for i in range(len(coins)-1,-1,-1):
+        if target>=coins2[i]:
+            cnt=target//coins2[i]
+            res+=cnt
+            target-=cnt*coins2[i]
+    return res
+
+coins=[10,5,2,1]
+target=3
+print(calc(coins,target))
