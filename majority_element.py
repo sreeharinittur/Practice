155689e7c49473af6968463ffece8ab0019b4ea2
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-02-01 16:52:35 +0530

    Add files via upload

---
+++ majority_element.py
@@ -0,0 +1,16 @@
+def majorityElement(nums):
+        n=len(nums)
+        a=n//3
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        l=[]
+        print(d)
+        for i,j in d.items():
+            if j>a:
+                l.append(i)
+        return l
+print(majorityElement([3,2,3]))
