78eca6be5da467326b59789df460911a7c8086b1
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-02-06 17:22:11 +0530

    Add files via upload

---
+++ bin_search.py
@@ -0,0 +1,20 @@
+a=[10,20,30,40]
+def binary_search(start1,end1,a,ele):
+    flag=0
+    n=len(a)
+    if n==0:
+        return None
+    if n==1:
+        return 0
+    if start1>end1:
+        return -1
+    mid=(start1+end1)//2
+    if a[mid]==ele:
+        flag=1
+        return mid
+    elif a[mid]<ele:
+        return binary_search(mid+1,end1,a,ele)
+    else:
+        return binary_search(start1,mid-1,a,ele)
+    
+print(binary_search(0,3,a,55))
