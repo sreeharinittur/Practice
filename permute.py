509ebc471575415f58f01f9a767156728cc72705
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-02-22 14:47:24 +0530

    Add files via upload

---
+++ permute.py
@@ -0,0 +1,10 @@
+def permute(s,chosen=""):
+    if len(s)==0:
+        print(chosen)
+        return
+    for i in range(len(s)):
+        ch=s[i]
+        remaining=s[:i]+s[i+1:]
+        permute(remaining,chosen+ch)
+word="ABC"
+print(permute(word))
