fce3e6bd995f4efecbe14d3924e0f96529cdf0c6
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-02-25 21:54:42 +0530

    Update dp3.py

--- dp3.py
+++ dp3.py
@@ -1,5 +1,5 @@
 def climb_stairs(n):
     if n<=2:
         return n
-    return climb_stairs(n-1)+climb_stairs(n-2)
+    return climb_stairs(n-1)+climb_stairs(n-2)//recursion
 print(climb_stairs(4))
