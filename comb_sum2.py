b803c0d6666d7ac5a514bdc240813b2fd30403c1
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-02-27 11:04:27 +0530

    Add files via upload

---
+++ comb_sum2.py
@@ -0,0 +1,24 @@
+def comb_sum(ind,arr,target,ans,ds):
+    if ind==len(arr):
+        if target==0:
+            ans.append(list(ds))
+        return
+    if arr[ind]<=target:
+        ds.append(arr[ind])
+        comb_sum(ind+1,arr,target-arr[ind],ans,ds)
+        ds.pop()
+    comb_sum(ind+1,arr,target,ans,ds)
+arr=[1,1,1,2,2]
+target=4
+ans=[]
+ds=[]
+ind=0
+comb_sum(ind,arr,target,ans,ds)
+print(ans)
+ans2=[]
+for i in ans:
+    if i not in ans2:
+        ans2.append(i)
+    else:
+        continue
+print(ans2)
