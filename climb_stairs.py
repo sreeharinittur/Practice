6c34cdba996ea44072d0ef8822ad2cfa5bd77ba0
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-03-12 20:28:42 +0530

    Add files via upload

---
+++ climb_stairs.py
@@ -0,0 +1,10 @@
+def climb_stairs(n):
+    if n==1:
+        return 1
+    if n==2:
+        return 2
+    if n==0:
+        return 1
+    return climb_stairs(n-1)+climb_stairs(n-2)
+    
+print(climb_stairs(4))
