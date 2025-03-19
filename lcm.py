20a71ebc2b5a067db1bb63d7f7ef8a1053732296
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-03-19 22:17:20 +0530

    Update lcm.py

--- lcm.py
+++ lcm.py
@@ -12,7 +12,7 @@ num2 = int(input("Enter second number: "))
 num3 = int(input("Enter third number: "))
 
 # Calculate LCM
-result = lcm_three(num1, num2, num3)
+result = lcm_three(num1, num2, num3)//calling the function
 
 # Display the result
 print("LCM of", num1, ",", num2, "and", num3, "is:", result)
