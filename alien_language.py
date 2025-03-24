c153f67844f79d2b4d8ecedb84045abddf98fcac
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-03-24 22:36:29 +0530

    Add files via upload

---
+++ alien_language.py
@@ -0,0 +1,13 @@
+
+def alien_language(n):
+    s=""
+    if n==1:
+        s+="C"
+    if n%2==0:
+        s=s+"A"+alien_language(n/2)
+        #s+alien_language(n/2)
+    if n>1 and n%2!=0:
+        s=s+"B"+alien_language(n-1)
+        
+    return s
+print(alien_language(6))
