842103d9f41fb03c55c5f01937d8d92148606637
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-04-07 22:46:53 +0530

    Add files via upload

---
+++ house_rob.py
@@ -0,0 +1,7 @@
+def house_rob(arr):
+    if len(arr)==1:
+        return arr[0]
+    if len(arr)==2:
+        return max(arr)
+    return max(arr[-1]+house_rob(arr[:-2]),house_rob(arr[:-1]))
+print( house_rob([5]))
