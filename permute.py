51e26218bef49890347c699a38f1b254d2da418f
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-04-22 12:46:21 +0530

    Update permute.py

--- permute.py
+++ permute.py
@@ -5,6 +5,6 @@ def permute(s,chosen=""):
     for i in range(len(s)):
         ch=s[i]
         remaining=s[:i]+s[i+1:]
-        permute(remaining,chosen+ch)
+        permute(remaining,chosen+ch)//recursion
 word="ABC"
 print(permute(word))
