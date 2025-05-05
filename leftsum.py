179b28c232269c12ec4d2c5cffe0a104050dab4f
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-05-05 18:30:12 +0530

    Update leftsum.py

--- leftsum.py
+++ leftsum.py
@@ -1,6 +1,6 @@
 # Definition for a binary tree node.
 class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=0, left=None, right=None)://constructor
         self.val = val
         self.left = left
         self.right = right
