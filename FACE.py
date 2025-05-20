f0b7e9645172607125e69e5ac8456d56d2a0ba51
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-05-20 22:21:52 +0530

    Update FACE.py

--- FACE.py
+++ FACE.py
@@ -1,6 +1,6 @@
 import pathlib
 import cv2 
-import os
+import os#import
 
 cascade_path=pathlib.Path(cv2.__file__).parent.absolute() / "data/haarcascade_frontalface_default.xml"
 print(cascade_path)//print
