a7e72b77b86b66657f498c00dd343bdd7fa964e8
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-05-26 22:20:15 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,6 +1,6 @@
 def factorial(n):
     if n == 0:
         return 1
-    return n * factorial(n - 1)//recursive function call
+    return n * factorial(n - 1)//recursive call of the function
 
 print(factorial(5))
