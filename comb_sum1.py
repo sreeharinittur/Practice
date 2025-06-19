e3ec5813a70e42936969ddc6b83b0597365a5136
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-06-19 23:03:00 +0530

    Update comb_sum1.py

--- comb_sum1.py
+++ comb_sum1.py
@@ -6,7 +6,7 @@ def find_comb(ind,arr,target,ans,ds):
     if arr[ind]<=target:
         ds.append(arr[ind])
         find_comb(ind,arr,target-arr[ind],ans,ds)
-        ds.pop()//pop element
+        ds.pop()#pop element from the ds
     find_comb(ind+1,arr,target,ans,ds)
 ind=0
 arr=[2,3,6,7]
