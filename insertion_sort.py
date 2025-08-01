bb4ca5bafa90b13f80d32498f818695b2e87b285
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-08-01 22:53:06 +0530

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,7 +4,7 @@ def i_sort(a):
         
         for j in range(i):
             if a[j]>a[i]://compare
-                a[i],a[j]=a[j],a[i]//swapping the elements 
+                a[i],a[j]=a[j],a[i]//swapping both
 
 
     return a//returning the answer
