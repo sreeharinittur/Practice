f2d513443ad5d5c8a5a0c0c2fe04c10eaec3c9ec
sreeharinittur <147301062+sreeharinittur@users.noreply.github.com>
2025-08-09 19:52:09 +0530

    Update comb_sum1.py

--- comb_sum1.py
+++ comb_sum1.py
@@ -9,9 +9,10 @@ def find_comb(ind,arr,target,ans,ds):
         ds.pop()#pop element from the ds
     find_comb(ind+1,arr,target,ans,ds)
 ind=0
-arr=[2,3,6,7]
+arr=[2,3,6,7]//array
 target=7
 ans=[]
 ds=[]
 find_comb(ind,arr,target,ans,ds)
 print(ans)
+
